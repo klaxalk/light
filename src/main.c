71f7755cb16c1bee689bbc5e2b2b1179670dda3f
Marco Wang <m.aesophor@gmail.com>
2019-08-28 15:38:15 +0800

    main: remove whitespace between if and left parenthesis

--- src/main.c
+++ src/main.c
@@ -11,12 +11,12 @@
 int main(int argc, char **argv)
 {
     light_context_t *light_ctx = light_initialize(argc, argv);
-    if (light_ctx == NULL) {
+    if(light_ctx == NULL) {
         LIGHT_ERR("Initialization failed");
         return LIGHT_RETURNVAL_INITFAIL;
     }
 
-    if (!light_execute(light_ctx)) {
+    if(!light_execute(light_ctx)) {
         LIGHT_ERR("Execution failed");
         light_free(light_ctx);
         return LIGHT_RETURNVAL_EXECFAIL;
